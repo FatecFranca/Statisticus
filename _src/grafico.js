7591d25a69b193d5a40893367cd447bb4f1419a7
arkannun <arkannun@gmail.com>
2019-05-21 15:36:28 -0300

    Grafico pronto falta tabela

--- _src/grafico.js
+++ _src/grafico.js
@@ -1,18 +1,84 @@
-function geraGrafico(ctx, tipo, titulo, dados, menorX, maiorX, menorY, maiorY, a, b ){
+let zoomX = 3;
+let zoomY = 3;
+let menorx=0;
+let maiorx=0
+let menory=0;
+let maiory=0;
+let graf;
+let titulo;
+let dados;
+let a=0;
+let b=0;
 
-    //////////////////////////////////////////////////////////////////////////
-    //////////// ARRUMAR OS VALORES DE MENOR E MAIOR E FAZELOS RELATIVOS A X e y\\\\\\\\\\\\\\\
-    ///////////////////////////////////////////////////////////////////////////
+function zoom(eixo,valor){
 
+    if(eixo=='x'){
+        zoomX+=valor;
+    }else{
+        zoomY+=valor;
+    }
+
+    graf.options = {
+        animation: false,
+        scales: {
+            responsive: true,
+            xAxes: [{
+                type: 'linear',
+                position: 'top',
+                ticks: {
+                    min: menorx-zoomX,
+                    max: maiorx+zoomX
+                }
+            }],
+            yAxes: [{
+                type: 'linear',
+                position: 'left',
+                ticks: {
+
+                    min: menory-zoomY,
+                    max: maiory+zoomY
+                }
+            }]
+        }
+    };
+
+    graf.data = {
+        datasets: [{
+            label: titulo,
+            data: dados,
+            borderColor: '#2196f3',
+            backgroundColor: '#2196f3',
+            borderWidth: 10,
+            showLine: false
+        }, {
+            type: 'line',
+            label: 'line',
+            data: [{x:menorx-zoomX-2, y:geraReta(menorx-zoomX-2,a,b)}, {x:maiorx+zoomX+2,y:geraReta(maiorx+zoomX+2,a,b)}],
+            borderColor: '#000000',
+            borderWidth: 5,
+            showLine: true,
+            fill: false
+
+        }]
+    }
+    graf.update();
+
+}
+
+function geraGrafico(ctx, tipo, Titulo, Dados, menorX, maiorX, menorY, maiorY, A, B ){
+    a=A;
+    b=B;
+    menorx=menorX;
+    menory=menorY;
+    maiorx=maiorX;
+    maiory=maiorY;
+    titulo=Titulo;
+    dados=Dados;
     if (tipo=='scatter'){
         tipo = 'line';
-        debugger;
-
-
-
-
-        var graf = new Chart(ctx, {
-            type: 'line', //tipo,
+        //debugger;
+        graf = new Chart(ctx, {
+            type: tipo,
             data: {
                 datasets: [{
                     label: titulo,
@@ -24,7 +90,7 @@ function geraGrafico(ctx, tipo, titulo, dados, menorX, maiorX, menorY, maiorY, a
                 }, {
                     type: 'line',
                     label: 'line',
-                    data: [{x:menorX-100, y:geraReta(menorX-100,a,b)}, {x:maiorX+100,y:geraReta(maiorX+100,a,b)}],
+                    data: [{x:menorx-zoomX-1, y:geraReta(menorx-zoomX-1,a,b)}, {x:maiorx+zoomX+1,y:geraReta(maiorx+zoomX+1,a,b)}],
                     borderColor: '#000000',
                     borderWidth: 5,
                     showLine: true,
@@ -36,10 +102,10 @@ function geraGrafico(ctx, tipo, titulo, dados, menorX, maiorX, menorY, maiorY, a
                 scales: {
                     xAxes: [{
                         type: 'linear',
-                        position: 'bottom',
+                        position: 'top',
                         ticks: {
-                            min: menorX,
-                            max: maiorX
+                            min: menorX-zoomX,
+                            max: maiorX+zoomX
                         }
                     }],
                     yAxes: [{
@@ -47,8 +113,8 @@ function geraGrafico(ctx, tipo, titulo, dados, menorX, maiorX, menorY, maiorY, a
                         position: 'left',
                         ticks: {
 
-                            min: geraReta(menorX,a,b),
-                            max: geraReta(maiorX,a,b)
+                            min: menorY-zoomY,
+                            max: maiorY+zoomY
                         }
                     }]
                 }
